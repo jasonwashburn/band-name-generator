90f849baced11ba98951ea46186399a6cea6b358
Jason Washburn <35488541+jasonwashburn@users.noreply.github.com>
2022-06-19 11:58:33 +0000

    adds python version

---
+++ python/band-name-generator.py
@@ -0,0 +1,8 @@
+def main():
+    street_name = input("What is the name of the street you grew up on? ")
+    pet_name = input("What is the name of your pet? ")
+    print(f"Your band name could be {street_name} {pet_name}!")
+    
+    
+if __name__ == "__main__":
+    main()
